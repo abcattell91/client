0be5a30256ee1410ef8a76f35ce2bcee615409b5
Alex Cattell <abcattell91@gmail.com>
2022-08-29 20:23:34 +0100

    added oauth to work in console with signed in and signed out changes to the text in header bar

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -16,8 +16,18 @@ class GoogleAuth extends React.Component {
     });
   }
 
+  renderAuthButton() {
+    if (this.state.isSignedIn === null ) {
+      return <div>I dont know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render () {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
