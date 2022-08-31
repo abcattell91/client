788da83dbf475c8629004f5bc03bd740e5694e64
Alex Cattell <abcattell91@gmail.com>
2022-08-31 18:39:17 +0100

    implemented the form element with redux-form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form'
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    )
+  }
+  render() {
+    // use FIELD when wanting to show a field to a user
+    return (
+      <form className="ui form">
+        <Field name="Title" component={this.renderInput} label="Enter Title" />
+        <Field name="Description" component={this.renderInput} label="Enter Description" />
+      </form>
+    )
+  };
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streameCreate'
+})(StreamCreate);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'; // done to rename the reducer to help know what it is.
 import authReducer from './authReducer';
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
