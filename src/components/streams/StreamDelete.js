0f4621cb9fe684b00d9e1e7e4af66964212dee2a
Alex Cattell <abcattell91@gmail.com>
2022-09-16 18:23:12 +0100

    interpolated title into delete modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Modal from '../Modal';
 import history from '../../history';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions'
 
 class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+  }
   renderActions() {
     return (
       <React.Fragment>
@@ -12,19 +17,27 @@ class StreamDelete extends React.Component {
     );
   }
 
+  renderContent () {
+    if (!this.props.stream) {
+      return 'Are you sure you want to delete this stream?';
+    }
+
+    return `Are you sure you want to delete this stream with Title:- ${this.props.stream.title}`
+  }
   render () {
     return (
-      <div>
-        StreamDelete
         <Modal
-          title="Delete Stream"
-          content="Are you sure you want to delete this stream?"
+          title={"Delete Stream"}
+          content={this.renderContent()}
           actions={this.renderActions()}
           onDismiss={() => history.push('/')}
         />
-      </div>
     )
   };
 };
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+return { stream: state.streams[ownProps.match.params.id] }
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamDelete);
