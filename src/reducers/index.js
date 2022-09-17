f16c360d70a68c26ffde1b51ef3a98fe68014ba8
Alex Cattell <abcattell91@gmail.com>
2022-09-17 19:46:56 +0100

    refactored to use react final form instead of redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form'; // done to rename the reducer to help know what it is.
 import authReducer from './authReducer';
 import streamReducer from './streamReducer';
 
 export default combineReducers({
   auth: authReducer,
-  form: formReducer,
-  streams: streamReducer
+  streams: streamReducer,
 });
